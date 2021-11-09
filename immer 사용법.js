808a5caeafa0983eb73314a16076693945ee1dfa
B H An <anbyunghoon0107@gamil.com>
2021-11-09 16:35:23 +0900

    immer 사용법

---
+++ immer 사용법.js
@@ -0,0 +1,45 @@
+// immer 사용법
+
+
+/* --------------------------------- 예시 코드 1 -------------------------------- */
+import produce from 'immer';
+
+const nextState = produce(originalState, draft => {
+    // 바꾸고 싶은 값 바꾸기
+    draft.somewhere.deep.inside = 5;
+})
+
+/*
+produce라는 함수는 두가지 파라미터를 받는다.
+    첫 번째 파라미터는 수정하고 싶은 상태이고, 
+    두 번째 파라미터는 상태를 어떻게 업데이트 할 지 정의하는 함수이다.
+*/
+
+
+/* -------------------------------- 예시 코드 2 -------------------------------- */
+
+const originalState = [
+    {
+        id: 1,
+        todo: '전개 연산자와 배열 내장 함수로 불변성 유지하기',
+        checked: false,
+    }
+];
+
+const nextState = produce(originalState, draft => {
+    // id가 2인 항목의 checked 값을 true로 설정
+    const todo = draft.find(t => t.id === 2); // id로 항목 찾기
+    todo.checked = true;
+    // 혹은 draft[1].checked = true;
+
+    // 배열에 새로운 데이터 추가
+    draft.push({
+        id: 3,
+        todo: '일정 관리 앱에 immer 적용하기',
+        checked: false,
+    });
+    // id = 1인 항목을 제거하기
+    draft.splice(draft.findIndex(t => t.id === 1), 1);
+})
+
+//draft??
